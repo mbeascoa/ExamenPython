09979e81c6c40f92e85c313baefc93395c6ed6ed
mbeascoa <miguel.beascoa@beastek.net>
2021-11-15 19:30:36 +0100

    version cutre sin clases

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 import sys
-from datetime import date
+
+miNombreHospital = "general"
+nuevoSalario=-1
 
 
-#----------------------------------------------------------------------
-def insertar():
+
+
+#----------ALTA DOCTOR ---------------------------------------------------------
+def altaDoctor():
+    hospitalcod=0
+    codigoHospital=0
+    # print("entrando en altaDoctor()")
+    nombreHospital = input("Introduzca el nombre del Hospital : \n")
+    #hospitalcod = hospitalnombre2code(nombreHospital)
+    #print(f'El código del Hospital es : {hospitalcod}')
+
     import cx_Oracle
-
     connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
-
     cursor = connection.cursor()
     try:
-        inscrip = int(input("Inserte el número del enfermo : "))
-        iapelli = input("Inserte el apellido del enfermo")
-        idir = input("Inserte la dirección del enfermo")
-        ifecnac= input("Inserte la fecha de nacimiento del enfermo")
-        isex= input("Inserte el sexo del enfermo")
-        inss = input("Inserte el Número de la Seguridad Social del enfermo")
 
-        cursor.callproc('tablaenfermo.insertar', (inscrip, iapelli, idir, ifecnac, isex, inss))
+        consulta = ("SELECT HOSPITAL_COD from HOSPITAL WHERE NOMBRE = :p1")
+
+        cursor.execute(consulta, (nombreHospital,))
+        # Si en un único parámetro tenemos que poner ',' a continuación del valor de la variable
+
+        cursor.execute(consulta)
+        codigoHospital = -1
+        resultado = False
+        for hospitalcode in cursor:
+            #print(f"Código Hospital , hospital_cod: , {hospitalcode}")
+            codigoHospital = hospitalcode[0]
+            resultado = True
+        if resultado == False:
+            print("Sin resultados")
+            codigoHospital = None
+    except connection.Error as error:
+        print("Error: ", error)
+   # print(f"Código Hospital , hospital_cod: , {codigoHospital}\n")
+    connection.close()
+
+    connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
+    cursor = connection.cursor()
+    try:
+        doctorno = input("Inserte el número del Doctor (doctorno)  : ")
+        apellido = input("Inserte el apellido del Doctor : ")
+        especialidad= input("Inserte la especialidad del Doctor : ")
+        salario= input("Inserte el salario del Doctor : ")
+
+
+
+        cursor.callproc('tabladoctor.altaDoctor', (codigoHospital, doctorno, apellido, especialidad, salario))
         print("DATO INSERTADO")
-#        ConsultaAlta = ("INSERT INTO ENFERMO "
-#                        "(INSCRIPCION, APELLIDO, DIRECCION, FECHA_NAC, SEXO, NSS) "
-#                        "VALUES (:P1, :P2, :P3, to_date(:P4, 'dd-mm-yyyy'), :P5, :P6)")
+    #        ConsultaAlta = ("INSERT INTO ENFERMO "
+    #                        "(INSCRIPCION, APELLIDO, DIRECCION, FECHA_NAC, SEXO, NSS) "
+    #                        "VALUES (:P1, :P2, :P3, to_date(:P4, 'dd-mm-yyyy'), :P5, :P6)")
 
-#        datosEnfermo = (inscrip, iapelli, idir, ifecnac, isex, inss)
-#        cursor.execute(ConsultaAlta, datosEnfermo)
+    #        datosEnfermo = (inscrip, iapelli, idir, ifecnac, isex, inss)
+    #        cursor.execute(ConsultaAlta, datosEnfermo)
         connection.commit()
 
     except connection.Error as error:
@@ -33,44 +66,30 @@ def insertar():
     cursor.close()
     connection.close()
 
-
-#--------------------------------------------------------------
-
-
-def salir():
-    sys.exit()
-
-
-#--------------------------------------------------------------
-
-
-def consultar():
+# -------------MODIFICAR SALARIO DOCTOR -------------
+def modificarSalarioDoctor():
     import cx_Oracle
+
     connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
+
     cursor = connection.cursor()
     try:
-        consulta = ("SELECT * FROM ENFERMO" )
-        cursor.execute(consulta)
 
-        resultado = False
-        for ideptno, inombre, idir, ifecnac, isex, inss in cursor:
-            print("Número Inscripción Enfermo: ", ideptno)
-            print("Nombre del Enfermo: ", inombre)
-            print("Dirección donde se ubica : ", idir)
-            print("Fecha de Nacimiento del enferno : ", ifecnac)
-            print("Sexo del enfermo : ", isex)
-            print("Número de la Seguridad Social del Enfermo: ", inss)
-            resultado = True
-        if resultado == False:
-            print("Sin resultados")
+        numDoctor = input("Inserte por favor el número del Doctor (Doctorno) :\n")
+        nuevoSalario=int(input("Inserte el nuevo salario algo razonable :\n"))
+        cursor.callproc('tabladoctor.modificarsalario', (numDoctor,nuevoSalario))
+
+        print("DATO MODIFICADO ")
+
     except connection.Error as error:
         print("Error: ", error)
-
+    cursor.close()
     connection.close()
 
-#--------------------------------------------------------------
+# ----------------ELIMINAR DOCTOR -------------------------------------------
 
-def borrar():
+def eliminarDoctor():
+
     import cx_Oracle
 
     connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
@@ -78,8 +97,8 @@ def borrar():
     cursor = connection.cursor()
     try:
 
-        numSS = input("NÚMERO DE Seguridad Social:")
-        cursor.callproc('tablaenfermo.borrar', (numSS,))
+        numDoctor = input("Inserte por favor el número del Doctor (Doctorno):")
+        cursor.callproc('tabladoctor.borrardoctor', (numDoctor,))
 
         print("DATO ELIMINADO")
 
@@ -88,67 +107,89 @@ def borrar():
     cursor.close()
     connection.close()
 
-"""
+# ----------------------------------------------------------------------------
 
-   import cx_Oracle
-
-    connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
-
-    cursor = connection.cursor()
-    try:
-
-        ConsultaBaja = ("Delete from enfermo where nss=:param1")
-
-        NumeroSS = input("Indiqueme el Número de la Seguridad Social para eliminar el enfermo:")
-
-        cursor.execute(ConsultaBaja, (NumeroSS,))
-        if cursor.rowcount > 0:
-            print("Enfermo eliminado satisfactoriamente")
-        else:
-            print("Dato no encontrado")
-
-        connection.commit()
-
-
-    except connection.Error as error:
-        print("Error: ", error)
-    cursor.close()
-    connection.close()
-
-"""
-
-def modificar():
-
-
+def mostrarApellidoEspecialidad():
     import cx_Oracle
-
     connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
-
     cursor = connection.cursor()
     try:
+        miNumeroDoctor = input("Introduce Número de Doctor:")
+        consulta = ("SELECT DOCTOR_NO, APELLIDO, ESPECIALIDAD FROM DOCTOR WHERE DOCTOR_NO= :p1")
+        cursor.execute(consulta, (miNumeroDoctor,))
 
-
-        NuevoDireccion = input("Nueva localización :")
-        NumeroSS = input(" Número de la Seguridad Social para poder modificar la dirección de este enfermo :")
-
-        cursor.callproc('tablaenfermo.cambiardireccion', (NumeroSS,NuevoDireccion))
-
-        if cursor.rowcount > 0:
-            print("Dirección modificada satisfactoriamente")
-        else:
-            print("Dato no encontrado")
-
-        connection.commit()
-
-
+        resultado = False
+        for doctno,ape, espe in cursor:
+            print("Número DOCTOR : ", doctno)
+            print("Apellido del Doctor: ", ape)
+            print("Especialidad del Doctor : ", espe)
+            resultado = True
+        if resultado == False:
+            print("Sin resultados")
     except connection.Error as error:
         print("Error: ", error)
-    cursor.close()
+
     connection.close()
-#---------------------------------------------------------
+
+
+"""------------------------------------------------------
+Vamos a crear una tabla nueva que se llama Grupo que tiene los datos de las tablas donde hay doctores y empleados, 
+
+CREATE TABLE GRUPO
+	(NOMBRE VARCHAR2(10),
+	TIPO VARCHAR2(10),
+	APELLIDO VARCHAR2(20),
+	SALARIO NUMBER(10,0));
 
 
 
+INSERT INTO GRUPO(NOMBRE, TIPO, APELLIDO, SALARIO)
+select h.nombre, 'plantilla' Tipo, p.apellido, p.salario
+from plantilla p
+join hospital h on p.hospital_cod = h.hospital_cod
+union
+select h.nombre, 'doctor' Tipo, d.apellido, d.salario
+from doctor d
+join hospital h on d.hospital_cod = h.hospital_cod
+order by 1,2,3
+
+
+# select  nombre, sum(salario), avg(salario), count(*) from grupo where NOMBRE = 'general' group by nombre
+"""
+
+def visualizarDatosGrupo():
+    import cx_Oracle
+    connection = cx_Oracle.connect("system", "Tardes", "localhost/XE")
+    cursor = connection.cursor()
+    try:
+        miNombreHospital = input("Introduce el nombre del Hospital: \n")
+        consulta = ("SELECT NOMBRE, SUM(salario), AVG(salario), COUNT(*) FROM GRUPO WHERE NOMBRE= :P1 GROUP BY NOMBRE")
+        cursor.execute(consulta, (miNombreHospital,))
+
+
+
+        resultado = False
+        for nom, sumasal, avgsal, countemp in cursor:
+            print("Datos del Grupo")
+            print("Nombre Hospital : ", nom)
+            print("Total Suma Salarial de Doctores y Empleados en Plantilla : ", sumasal)
+            print("Media Salarial del Hospital : ", avgsal)
+            print("Número de Empleados, Doctores y Plantilla : ", countemp)
+            resultado = True
+        if resultado == False:
+            print("Sin resultados")
+    except connection.Error as error:
+        print("Error: ", error)
+
+    connection.close()
+
+
+#----------SALIR  ------------------------------------------------------------
+
+def salir():
+    sys.exit()
+
+# ------------------------------MENU PRINCIPAL --------------------------------
 quiereIntentarlo=True
 letra="S"
 
@@ -158,27 +199,28 @@ while quiereIntentarlo :
             print( "1.- Alta Doctor ")
             print("2 .- Modificar salario Doctor ")
             print("3 .- Eliminar Doctor")
-            print("4.- Mostrar Apellido y especialidad de un Doctor ")
-            print("5.- Visualizar datos de grup de un Hospital")
+            print("4.- Mostrar apellido y especialidad de un Doctor ")
+            print("5.- Visualizar datos de grupo de un Hospital")
             print("6.- Salir ")
             print(" ---------------------------------- ")
             opcion = input("Introduzca la opción requerida :  \n")
             if opcion=="1":
-                insertar()
+                altaDoctor()
             if opcion=="2":
-                borrar()
+                modificarSalarioDoctor()
             if opcion == "3":
-                modificar()
+                eliminarDoctor()
             if opcion == "4":
-                consultar()
+                mostrarApellidoEspecialidad()
             if opcion == "5":
+                visualizarDatosGrupo()
+            if opcion == "6":
                 salir()
             print(" ----------------------------------------------")
             intento = input("¿Quiere volver a intentarlo? (S/N)").upper()
             if intento=="S" or intento=="s" :
                 quiereIntentarlo=True
+                letra="S"
             else:
                 quiereIntentarlo=False
-
-
-
+                letra="N"
